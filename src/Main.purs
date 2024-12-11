a2099273b47fe7387ca3ae0ebb6fc852a1d79bbe
yukikurage <55534323+yukikurage@users.noreply.github.com>
2024-12-11 22:09:25 +0900

    log を変更

--- src/Main.purs
+++ src/Main.purs
@@ -4,6 +4,7 @@ import Prelude
 
 import AST (parseProgramWithErr, runProgram)
 import Data.Either (Either(..))
+import Data.Foldable (for_)
 import Effect (Effect)
 import Effect.Console (log)
 import Parsing (runParser)
@@ -12,8 +13,10 @@ main :: Effect Unit
 main = do
   case runParser "1 + 2; 2 * (3 + 4); 2 * 3; " parseProgramWithErr of
     Left err -> log $ "Parse Error: " <> show err
-    Right ast -> log $ show $ runProgram ast
+    Right ast -> for_ (runProgram ast) (log <<< show)
+
+  log ""
 
   case runParser "1 + 2; 2 ** (3 + 4); 2 * 3; " parseProgramWithErr of
     Left err -> log $ "Parse Error: " <> show err
-    Right ast -> log $ show $ runProgram ast
+    Right ast -> for_ (runProgram ast) (log <<< show)
